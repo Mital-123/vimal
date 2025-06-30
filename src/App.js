5a089756a214664dd9cc5f795f367ac893bf21bb
tankvaishali <vaishalitank28603@gmail.com>
2025-06-30 09:57:17 +0530

    give about path in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './Component/Home/HomePage';
 import ContactPage from './Component/ContactUs/ContactPage';
+import AboutusSec from './Component/Aboutus/AboutusSec';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/aboutus' element={<AboutusSec />} />
           <Route path='/contact' element={<ContactPage />} />
         </Routes>
       </BrowserRouter>
